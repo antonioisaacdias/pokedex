f911453df43ced2c6fd2208df0b25ac98f8dcac7
aliciaafoliveira <alicia.alexandra@cs.unipe.edu.br>
2024-11-22 19:55:15 -0300

    Estrutura de classe tipo

---
+++ src/service/Database_Manager.java
@@ -0,0 +1,13 @@
+package service;
+
+import com.sun.jdi.connect.spi.Connection;
+
+public class Database_Manager {
+    public Connection connection;
+    private String url;
+    private String user;
+    private String password;
+
+    public void connect(){}
+    public void disconnect(){}
+}
